5ddc5606d3debfd50dda9fa434b7c0017f8f4a9d
Diego <diegomugnae@gmail.com>
2019-10-15 13:20:24 +1000

    Get Json info
    Geting json info from https://docs.airnowapi.org/webservices

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=380EB5C4-750F-4897-B7BA-DC8DDD241D5A")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "Error..."
+
+
+	return render(request, 'home.html', {'api': api} )
+	
 
 def about(request):
 	return render(request, 'about.html', {})
+
