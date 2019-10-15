9e1cd7208878258074dcf576586e9ee789707069
Diego <diegomugnae@gmail.com>
2019-10-15 14:45:57 +1000

    configure form and post method
    Configuring form to request a new zipcode also added csrf token

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -5,35 +5,80 @@ def home(request):
 	import json
 	import requests
 
-	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=380EB5C4-750F-4897-B7BA-DC8DDD241D5A")
+	if request.method == "POST":
+		zipcode = request.POST['zipcode']
+		api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+ zipcode +"&distance=5&API_KEY=380EB5C4-750F-4897-B7BA-DC8DDD241D5A")
 
-	try:
-		api = json.loads(api_request.content)
-	except Exception as e:
-		api = "Error..."
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+		try:
+			api = json.loads(api_request.content)
+		except IndexError as e:
+			api = "Error..."						
 
-	if api[0]['Category']['Name'] == "Good":
-		category_description = "(0 to 50) Air quality is considered satisfactory, and air pollution poses little or no risk."
-		category_color = "good"
-	elif api[0]['Category']['Name'] == "Moderate":	
-		category_description = "(51 to 100)	Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
-		category_color = "moderate"
-	elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
-		category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is not likely to be affected."
-		category_color = "usg"
-	elif api[0]['Category']['Name'] == "Unhealthy":
-		category_description = "(151 to 200) Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
-		category_color = "unhealthy"
-	elif api[0]['Category']['Name'] == "Very Unhealthy":
-		category_description = "(201 to 300) Health alert: everyone may experience more serious health effects."
-		category_color = "veryunhealthy"
-	elif api[0]['Category']['Name'] == "Hazardous":
-		category_description = "(301 to 500) Health warnings of emergency conditions. The entire population is more likely to be affected."
-		category_color = "hazardous"
-	   
+		if api[0]['Category']['Name'] == "Good":
+			category_description = "(0 to 50) Air quality is considered satisfactory, and air pollution poses little or no risk."
+			category_color = "good"
+		elif api[0]['Category']['Name'] == "Moderate":	
+			category_description = "(51 to 100)	Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+			category_color = "moderate"
+		elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+			category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+			category_color = "usg"
+		elif api[0]['Category']['Name'] == "Unhealthy":
+			category_description = "(151 to 200) Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+			category_color = "unhealthy"
+		elif api[0]['Category']['Name'] == "Very Unhealthy":
+			category_description = "(201 to 300) Health alert: everyone may experience more serious health effects."
+			category_color = "veryunhealthy"
+		elif api[0]['Category']['Name'] == "Hazardous":
+			category_description = "(301 to 500) Health warnings of emergency conditions. The entire population is more likely to be affected."
+			category_color = "hazardous"
+		   
 
 
-	return render(request, 'home.html', {'api': api, 'category_description': category_description, 'category_color': category_color} )
+		return render(request, 'home.html', {
+			'api': api,
+			'category_description': category_description,
+			'category_color': category_color
+			} )
+
+	else:
+		api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=380EB5C4-750F-4897-B7BA-DC8DDD241D5A")
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+
+		if api[0]['Category']['Name'] == "Good":
+			category_description = "(0 to 50) Air quality is considered satisfactory, and air pollution poses little or no risk."
+			category_color = "good"
+		elif api[0]['Category']['Name'] == "Moderate":	
+			category_description = "(51 to 100)	Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+			category_color = "moderate"
+		elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+			category_description = "(101 to 150) Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+			category_color = "usg"
+		elif api[0]['Category']['Name'] == "Unhealthy":
+			category_description = "(151 to 200) Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+			category_color = "unhealthy"
+		elif api[0]['Category']['Name'] == "Very Unhealthy":
+			category_description = "(201 to 300) Health alert: everyone may experience more serious health effects."
+			category_color = "veryunhealthy"
+		elif api[0]['Category']['Name'] == "Hazardous":
+			category_description = "(301 to 500) Health warnings of emergency conditions. The entire population is more likely to be affected."
+			category_color = "hazardous"
+		   
+
+
+		return render(request, 'home.html', {
+			'api': api,
+			'category_description': category_description,
+			'category_color': category_color
+			} )
 	
 
 def about(request):
